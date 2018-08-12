9fae75741d84457560fbc9a8ecd63e4ca1ced74c
Yuta Kitagawa <y.kitagawa.r@outlook.jp>
2018-08-12 20:53:26 +0900

    shallwe配下のswiftファイルのみ移行part3

--- shallWe/TabBarController.swift
+++ shallWe/TabBarController.swift
@@ -1,17 +1,16 @@
-//
-//  TabBarController.swift
-//  shallWe
-//
-//  Created by 上山　俊佑 on 2018/08/11.
-//  Copyright © 2018年 Shunsuke Ueyama. All rights reserved.
-//
-
-import UIKit
-import Firebase
-import SDWebImage
-
-class TabBarController: UITabBarController {
-
+ //  TabBarController.swift
+ //  shallWe
+ //
+ //  Created by 上山　俊佑 on 2018/08/11.
+ //  Copyright © 2018年 Shunsuke Ueyama. All rights reserved.
+ //
+ 
+ import UIKit
+ import Firebase
+ import SDWebImage
+ 
+ class TabBarController: UITabBarController {
+    
     //LoginViewControllerからパラメーターを取得する
     var uid = Auth.auth().currentUser?.uid
     var profileImage:NSURL!
@@ -28,15 +27,15 @@ class TabBarController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+ }
